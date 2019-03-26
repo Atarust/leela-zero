0d289c507649b491463c7ba7a1df5dff814fe09c
Jonas Kapitzke <mail@jkapitzke.de>
2019-03-26 18:28:31 +0100

    add fairness factor

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -281,10 +281,10 @@ UCTNode* UCTNode::uct_select_child(int color, bool is_root) {
         //auto fairness_cost = (0.01 * child.get_visits());
 
         
-        auto fairness_cost = pow(1.005, pow(child.get_visits(), 0.5)) - 1;
+        auto fairness_cost = pow(1.05, pow(child.get_visits(), 0.5)) - 1;
         
         fairness_cost = (is_root ? fairness_cost : 0);
-        const auto value = winrate + puct;
+        const auto value = winrate + puct - fairness_cost;
         assert(value > std::numeric_limits<double>::lowest());
 
         if (value > best_value) {
